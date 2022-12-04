63d3bd3ce37ae99775e12b7a171a706529ef17f3
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-04 21:21:35 +0530

    courseview

---
+++ src/app/courseview/courseview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courseview',
+  templateUrl: './courseview.component.html',
+  styleUrls: ['./courseview.component.css']
+})
+export class CourseviewComponent {
+  data:any=[]
+
+}
