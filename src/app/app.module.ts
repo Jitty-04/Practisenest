28d946606dadf59bbb31fdb06b65439986b33d95
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-04 21:14:14 +0530

    adminhome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { FormsModule } from '@angular/forms';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    AdminhomeComponent
   ],
   imports: [
     BrowserModule,
